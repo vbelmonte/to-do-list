d06771940b1eab228a95b205c8accda7de89f3d6
Villy Belmonte <vjbbelmont@gmail.com>
2023-04-19 18:00:55 -0700

    Add hover effects and event listener side column logo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import loadInbox from './inbox';
 
 (function runPage() {
   const inboxDesktop = document.getElementById('inbox');
+  const logo = document.getElementsByClassName('logo')[0];
 
+  logo.addEventListener('click', loadInbox);
   inboxDesktop.addEventListener('click', loadInbox);
 }());
